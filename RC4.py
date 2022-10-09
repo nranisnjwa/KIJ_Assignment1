f789257b295e928e9443004742217305a7813841
nranisnjwa <114972542+nranisnjwa@users.noreply.github.com>
2022-10-09 19:46:02 +0800

    Add files via upload

---
+++ RC4.py
@@ -0,0 +1,24 @@
+from telnetlib import ENCRYPT
+from Crypto.Cipher import ARC4
+from Crypto.Hash import SHA
+from Crypto.Random import get_random_bytes
+
+
+
+def enc(key,plaintext):
+		return ARC4.new(key).encrypt(plaintext)
+
+def dec(key,msg):
+		return ARC4.new(key).decrypt(msg)
+
+
+key = b'Attack On Titan!'
+plaintext =b'Halo Eren Yeager'
+nonce = get_random_bytes(16)
+tempkey = SHA.new(key+nonce).digest()
+cipher = ARC4.new(tempkey)
+msg = nonce + cipher.encrypt(plaintext)
+
+
+print(plaintext)
+print(msg)
