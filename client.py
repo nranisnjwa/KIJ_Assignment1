0b543169c744b6f5cf91dab9ebb421bf13112d98
Nurul Nabilah <115098781+hijklewww@users.noreply.github.com>
2022-10-10 12:31:40 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,31 @@
+import socket
+import tqdm
+import os
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096
+
+host = "192.168.1.101"
+port = 5001
+filename = "data.csv"
+filesize = os.path.getsize(filename)
+
+s = socket.socket()
+
+print(f"[+] Connecting to {host} : {port}")
+s.connect ((host, port))
+print("[+] Connected.")
+
+s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+
+progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit-"B", unit_scale=True, unit_divisor=1024)
+with open(filename, "rb") as f:
+
+    while True:
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            break
+        s.sendall(bytes_read)
+        progress.update(len(bytes_read))
+
+s.close()
